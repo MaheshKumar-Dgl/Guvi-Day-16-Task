c694550a1662969f4fb59b9c58c3bcafb672df46
Maheshkumar A <115472051+MaheshKumar-Dgl@users.noreply.github.com>
2024-05-04 12:40:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var timer = document.getElementById('sec');
+timer.innerHTML = countdown--;
+setTimeout(() => {
+    timer.innerText = countdown--;
+    setTimeout(() => {
+        timer.innerText = countdown--;
+        setTimeout(() => {
+            timer.innerText = countdown--;
+            setTimeout(() => {
+                timer.innerText = countdown--;
+                setTimeout(() => {
+                    timer.innerText = countdown--;
+                    setTimeout(() => {
+                        timer.innerText = countdown--;
+                        setTimeout(() => {
+                            timer.innerText = countdown--;
+                            setTimeout(() => {
+                                timer.innerText = countdown--;
+                                setTimeout(() => {
+                                    timer.innerText = countdown--;
+                                    setTimeout(() => {
+                                        timer.innerText = countdown--;
+                                        document.getElementById('timer-content').style.display = 'none';
+                                        document.getElementById('wishes-content').style.display = 'block';
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000);
